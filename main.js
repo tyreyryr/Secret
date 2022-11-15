d894d770a81b85390c342770d20684bc7a758da3
tyreyryr <81716468+tyreyryr@users.noreply.github.com>
2022-11-15 15:59:27 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function validate() {
+    var username = document.getElementById("Username").value;
+    var passowrd = document.getElementById("Password").value;
+    var ErrorMessage = document.getElementById("Error")
+    var SuccessfulMessage = document.getElementById("Successfuly")
+    if (username=="Bulgatian"&& passowrd=="RP") {
+        SuccessfulMessage.style.opacity=1;
+        setTimeout(function () {
+            window.open("");
+            window.close("");
+            SuccessfulMessage.style.opacity=0;
+        }, 1000);
+        return false;
+    }else{
+        ErrorMessage.style.opacity=1;
+        setTimeout(function () {
+            ErrorMessage.style.opacity=0;
+        }, 1000);
+        
+    }
+
+}
